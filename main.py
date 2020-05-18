926e617cf2a7baef8ea3bbe30ad3693bd0538393
Primal System Studio <primalsystemstudio@protonmail.com>
2020-05-18 18:57:34 +0400

    Added class to handle successful and unsuccessful login.

--- main.py
+++ main.py
@@ -4,6 +4,7 @@
 # ---------------------------------------------------------- #
 
 # Export as pyinstaller main.py -F
+# Username and password are both "admin"
 
 hours = 7
 
@@ -14,19 +15,41 @@ import login, landing, student_entry, view_students, report
 # Define QT5 app
 
 app = QtWidgets.QApplication(sys.argv)
-dialog = QtWidgets.QDialog()
+
+# Define login dialogue
+
+class Login(QtWidgets.QDialog):
+    def __init__(self):
+        super().__init__()
+        self.ui = login.Ui_Dialog()
+        self.ui.setupUi(self)
+        self.show()
+        
+        # Check if username/password combo is correct, if it is open landing page
+        if (self.exec_() == QtWidgets.QDialog.Accepted and self.ui.usernameLine.text() == 'admin' and self.ui.passwordLine.text() == 'admin'):
+            self.close()
+        else:
+            warn = QtWidgets.QMessageBox()
+            warn.setIcon(QtWidgets.QMessageBox.Warning)
+            warn.setText(
+               "Username and password combination is incorrect, try again")
+            warn.setStandardButtons(
+                QtWidgets.QMessageBox.Ok | QtWidgets.QMessageBox.Cancel)
+            warn.exec()
+            self.ui.setupUi(self)
+            self.show()
+            
+class Landing(QtWidgets.QDialog):
+    def __init__(self):
+        super().__init__()
+        self.ui = landing.Ui_MainWindow()
+        self.ui.setupUi(self)
+        self.show
+    
+# Display login dialog
+window = Login()
+window.show
 
 
-# Display login dialogue
-
-logger = login.Login()
-logger.setupUi(dialog)
-dialog.show()
-
-
-
-lander = landing.Ui_MainWindow()
-lander.setupUi(mainWindow)
-# mainWindow.show()
 
 sys.exit(app.exec_())
